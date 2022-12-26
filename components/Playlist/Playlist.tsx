ea573647687ac4c81d53edd662cbad665c27cc91
paulohyst <oluaptarsso@gmail.com>
2022-12-26 20:13:52 -0300

    feat(audio player): add playlist components

---
+++ components/Playlist/Playlist.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useSelector } from "react-redux";
+import { RootState } from "store/store";
+import {
+  DragDropContext,
+  Draggable,
+  DraggableProvided,
+  Droppable,
+  DroppableProvided,
+  DropResult,
+} from "react-beautiful-dnd";
+import PlaylistItem from "./components/PlaylistItem";
+import styles from "./Playlist.module.scss";
+import { useDispatch } from "react-redux";
+import { reorderPlaylist } from "providers/audioPlayerSlice";
+
+export type PlaylistProps = {
+  className?: string;
+};
+
+const Playlist: React.FC<PlaylistProps> = ({ className }) => {
+  const { playlist, currentMusic } = useSelector(
+    (state: RootState) => state.audioPlayerStore
+  );
+
+  const dispatch = useDispatch();
+
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    dispatch(
+      reorderPlaylist({ startIndex: source.index, endIndex: destination.index })
+    );
+  };
+
+  return (
+    <div className={`${className} ${styles.playlistContainer}`}>
+      <h2>Playlist</h2>
+      {playlist.length ? (
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="playlist">
+            {(
+              provided: DroppableProvided
+            ) => (
+              <ul ref={provided.innerRef} {...provided.droppableProps}>
+                {playlist.map((music, i) => (
+                  <Draggable
+                    key={music.id.toString()}
+                    draggableId={music.id.toString()}
+                    index={i}
+                  >
+                    {(
+                      providedDraggable: DraggableProvided,
+                    ) => (
+                      <li
+                        className={styles.playlistItemContainer}
+                        ref={providedDraggable.innerRef}
+                        {...providedDraggable.draggableProps}
+                        {...providedDraggable.dragHandleProps}
+                      >
+                        <PlaylistItem
+                          music={music}
+                          playing={music.id === currentMusic?.music.id}
+                        />
+                      </li>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+      ) : (
+        <div className={styles.emptyPlaylist}>Play or add music to the playlist</div>
+      )}
+    </div>
+  );
+};
+
+export default Playlist;
---
+++ components/Playlist/components/PlaylistItem.tsx
@@ -0,0 +1,48 @@
+import {
+  removeFromPlaylist,
+  playMusic,
+} from "providers/audioPlayerSlice";
+import { IMusic } from "hooks/useAvailableMusics";
+import { IoCloseOutline, IoPlayOutline } from "react-icons/io5";
+import { useDispatch } from "react-redux";
+import ActionButton from "components/ActionButton/ActionButton";
+import styles from "./PlaylistItem.module.scss";
+
+export type PlaylistItemProps = {
+  className?: string;
+  music: IMusic;
+  playing : boolean
+};
+
+const PlaylistItem: React.FC<PlaylistItemProps> = ({ className, music, playing }) => {
+  const dispatch = useDispatch();
+
+  const remove = () => {
+    dispatch(removeFromPlaylist(music));
+  };
+
+  const playThisMusic = () => {
+    dispatch(playMusic({ music, fromPlaylist: true }));
+  };
+
+  return (
+    <div className={`${className} ${styles.playlistItem}`}>
+      <div className={`${playing ? styles.playing : ''} ${styles.content}`}>
+        <span className={styles.author}>{music.author}</span>-
+        <span className={styles.title}>{music.name}</span>
+      </div>
+      <div className={styles.actions}>
+        <ActionButton
+          onClick={playThisMusic}
+          icon={IoPlayOutline}
+        ></ActionButton>
+        <ActionButton
+          onClick={remove}
+          icon={IoCloseOutline}
+        ></ActionButton>
+      </div>
+    </div>
+  );
+};
+
+export default PlaylistItem;
