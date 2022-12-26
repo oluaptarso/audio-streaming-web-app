2c601b4b6c98d27674cff0c71a6e812774021ac7
paulohyst <oluaptarsso@gmail.com>
2022-12-26 20:14:58 -0300

    feat(app): use custom font, and providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
+import { AudioProvider } from "../providers/AudioPlayerProvider";
+import { Roboto } from "@next/font/google";
+
+const customNextFont = Roboto({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <AudioProvider>
+        <style jsx global>
+          {`
+            :root {
+              --custom-next-font: ${customNextFont.style.fontFamily};
+            }
+          `}
+        </style>
+        <Component {...pageProps} />
+      </AudioProvider>
+    </Provider>
+  );
 }
